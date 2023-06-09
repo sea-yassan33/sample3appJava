c4faf42011b978e4fe22c89ae0ae0d7fdd3608a1
sea-yassan33 <a.yasuda_tr51@me.com>
2023-06-09 14:44:42 +0900

    first_commit

---
+++ src/main/java/com/example/sample3app/Sample3appApplication.java
@@ -0,0 +1,13 @@
+package com.example.sample3app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sample3appApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sample3appApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sample3app/Sample3appApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sample3app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sample3appApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
